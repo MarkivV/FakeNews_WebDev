dcc03c1eba37d6918d64823b6984e0a238ef52d8
Vladimir Markiv <vladimir@Vladimirs-MacBook-Pro.local>
2023-03-01 22:38:18 +0100

    Image Optimization

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Layout from "../layouts/Layout";
 import {SessionProvider} from "next-auth/react";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <SessionProvider session={pageProps.session}>
           <Layout>
               <Component {...pageProps} />
+              <Analytics/>
           </Layout>
       </SessionProvider>
   )
